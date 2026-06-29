2a89a045a5ddf0ccc38dbd8b1f155819b628f0fc
Z User <z@container>
2026-06-29 23:13:47 +0000

    fix: force new build with Vault success banner + build-info.json
    The Vercel CDN was aggressively caching the old build. By changing main.jsx
    and adding build-info.json, the build hash changed from index-CBzKhYgQ.js
    to index-DK7XQtyS.js, forcing the CDN to serve the new code.
    
    Now the Vault page will:
    - Detect ?success=true&skillId=X&sessionId=Y from Stripe redirect
    - Save purchase to localStorage
    - Show '✅ PAYMENT SUCCESSFUL!' banner
    - Work even if user is not authenticated

--- aep-marketplace/src/main.jsx
+++ aep-marketplace/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// MarketNow v2.1.1 - Stripe fix + Vault success banner
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
-// Force rebuild Mon Jun 29 23:02:29 UTC 2026
